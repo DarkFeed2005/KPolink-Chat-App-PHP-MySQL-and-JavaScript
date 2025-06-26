cd0910ec987f18d23dccb43363d1083c59d1948f
DarkFeed2005 <kalanayasassrisathruwan123@gmail.com>
2025-06-26 23:25:49 +0530

    index.php

--- index.php
+++ index.php
@@ -5,3 +5,22 @@
   }
 ?>
 
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form signup">
+      <header>KPolink</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="name-details">
+          <div class="field input">
+            <label>First Name</label>
+            <input type="text" name="fname" placeholder="First name" required>
+          </div>
+          
+
+  <script src="javascript/pass-show-hide.js"></script>
+  <script src="javascript/signup.js"></script>
+
+</body>
+</html>
