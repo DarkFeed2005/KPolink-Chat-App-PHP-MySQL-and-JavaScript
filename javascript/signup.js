0e889d81e322299a411e22111487f02871370abe
DarkFeed2005 <kalanayasassrisathruwan123@gmail.com>
2025-06-30 22:25:31 +0530

    signup

--- javascript/signup.js
+++ javascript/signup.js
@@ -25,3 +25,5 @@ continueBtn.onclick = ()=>{
     let formData = new FormData(form);
     xhr.send(formData);
 }
+// This code handles the signup form submission. It prevents the default form submission behavior, sends an AJAX request to the server with the form data, and handles the response to either redirect to the users page or display an error message.
+// The form data is sent using the FormData object, which automatically encodes the form fields. The server response is checked, and if it indicates success, the user is redirected to the users page. If there is an error, it displays the error message in the designated error text element. 
